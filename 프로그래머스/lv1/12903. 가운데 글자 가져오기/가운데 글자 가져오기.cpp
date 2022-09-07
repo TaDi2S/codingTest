49489725186134903aade2f9eb96e918547f466d
TaDi2S <68476583+TaDi2S@users.noreply.github.com>
2022-09-07 15:53:13 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 3.93 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int length = s.size();
+    
+    if (length % 2 == 0) {
+        answer = s[length / 2 - 1];
+        answer += s[length / 2];
+    }
+    else {
+        answer = s[length / 2];
+    }
+
+    return answer;
+}
